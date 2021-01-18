c463fb40513d0e411c77b672101e1487a6ab2e4c
Ronit Dhingra <rdhingra001@hotmail.com>
2021-01-18 12:01:28 -0500

    initial project setup and creation

---
+++ BuddieyiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuddieyiOS
+//
+//  Created by  Ronit D. on 1/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
